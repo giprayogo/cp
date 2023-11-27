4852dcc6f7a3a03495fa0d3606b01e931e268e9c
giprayogo <genki016@gmail.com>
2023-11-27 10:20:30 +0900

    Added adv-2022-4

---
+++ src/bin/adv-2022-4.rs
@@ -0,0 +1,33 @@
+use std::collections::HashSet;
+use std::io;
+use std::str::FromStr;
+
+fn main() {
+    let lines = io::stdin().lines().flatten();
+    let mut covers = 0;
+    for line in lines {
+        if line.is_empty() {
+            continue;
+        }
+        let (elf_a, elf_b) = match &line
+            .split(',')
+            .map(|e| e.split('-'))
+            .map(|e| match e.clone().collect::<Vec<_>>()[..] {
+                [start, stop] => {
+                    let start = i32::from_str(start).unwrap();
+                    let stop = i32::from_str(stop).unwrap();
+                    (start..=stop).collect::<HashSet<_>>()
+                }
+                _ => panic!("Invalid dash split: {:?}", e),
+            })
+            .collect::<Vec<_>>()[..]
+        {
+            [a, b] => (a.clone(), b.clone()),
+            _ => panic!("Invalid comma split: {}", line),
+        };
+        if elf_a.is_superset(&elf_b) || elf_a.is_subset(&elf_b) {
+            covers += 1;
+        }
+    }
+    println!("bad: {covers}")
+}

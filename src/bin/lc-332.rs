7aed65f4737a1d2d9d7c59c0550a6bbc23883209
giprayogo <genki016@gmail.com>
2024-03-18 17:00:29 +0900

    Add first working solution for lc-332

--- src/bin/lc-332.rs
+++ src/bin/lc-332.rs
@@ -40,14 +40,14 @@ impl Solution {
             n_trips: usize,
             i: usize,
             j: usize,
-        ) {
+        ) -> bool {
             let used = match counter.get(&(i, j)) {
                 Some(v) => *v,
                 None => 0,
             };
             if adj_matrix[i][j] == 0 || used >= adj_matrix[i][j] {
+                false
             } else {
-                // println!("{i},{j}");
                 f.push((i, j));
                 counter.entry((i, j)).and_modify(|e| *e += 1).or_insert(1);
                 if f.len() == n_trips {
@@ -56,102 +56,119 @@ impl Solution {
                         it.push(_f.1);
                     }
                     itineraries.push(it);
+                    return true;
                 }
                 for k in 0..adj_matrix.len() {
-                    dfs(adj_matrix, f, itineraries, counter, n_trips, j, k);
+                    let quit = dfs(adj_matrix, f, itineraries, counter, n_trips, j, k);
+                    if quit {
+                        //ad-hoc short circuit!
+                        return true;
+                    }
                 }
                 f.pop();
                 counter.entry((i, j)).and_modify(|e| *e -= 1);
+                false
             }
         }
 
         let mut f = Vec::new();
         let mut itineraries = Vec::new();
         // TODO: small opt: can just use the number index
-        for v in adj_list.get("JFK").unwrap() {
-            let i = *rev_idx.get("JFK").unwrap();
-            let j = *rev_idx.get(v).unwrap();
-            dfs(
-                &adj_matrix,
-                &mut f,
-                &mut itineraries,
-                &mut HashMap::new(),
-                n_trips,
-                i,
-                j,
-            );
+        let mut adjs = adj_list.get("JFK").unwrap().clone();
+        // NOTE: After reading Eulerian path (discussion @ leetcode):
+        // I realize if I choose greedily by lowest lex ordering, I don't need
+        // to find every possible path!
+        // Lesson: read the problem properly; what being asked and what consequence of the chosen path
+        adjs.sort();
+        let i = *rev_idx.get("JFK").unwrap();
+        // for v in adjs {
+        for j in 0..n {
+            // let j = *rev_idx.get(&v).unwrap();
+            if adj_matrix[i][j] > 0 {
+                let yea = dfs(
+                    &adj_matrix,
+                    &mut f,
+                    &mut itineraries,
+                    &mut HashMap::new(),
+                    n_trips,
+                    i,
+                    j,
+                );
+                if yea {
+                    return itineraries[0]
+                        .iter()
+                        .map(|x| idx.get(x).unwrap().clone().clone())
+                        .collect();
+                }
+            }
         }
-
-        itineraries.sort();
-        itineraries[0]
-            .iter()
-            .map(|x| idx.get(x).unwrap().clone().clone())
-            .collect()
+        // }
+        unreachable!()
     }
 }
 
 fn main() {
-    // assert_eq!(
-    //     Solution::find_itinerary(vec![
-    //         vec!["MUC".to_string(), "LHR".to_string()],
-    //         vec!["JFK".to_string(), "MUC".to_string()],
-    //         vec!["SFO".to_string(), "SJC".to_string()],
-    //         vec!["LHR".to_string(), "SFO".to_string()]
-    //     ]),
-    //     vec![
-    //         "JFK".to_string(),
-    //         "MUC".to_string(),
-    //         "LHR".to_string(),
-    //         "SFO".to_string(),
-    //         "SJC".to_string()
-    //     ]
-    // );
-    // assert_eq!(
-    //     Solution::find_itinerary(vec![
-    //         vec!["JFK".to_string(), "SFO".to_string()],
-    //         vec!["JFK".to_string(), "ATL".to_string()],
-    //         vec!["SFO".to_string(), "ATL".to_string()],
-    //         vec!["ATL".to_string(), "JFK".to_string()],
-    //         vec!["ATL".to_string(), "SFO".to_string()]
-    //     ]),
-    //     vec![
-    //         "JFK".to_string(),
-    //         "ATL".to_string(),
-    //         "JFK".to_string(),
-    //         "SFO".to_string(),
-    //         "ATL".to_string(),
-    //         "SFO".to_string()
-    //     ]
-    // );
-    // assert_eq!(
-    //     Solution::find_itinerary(vec![vec!["JFK".to_string(), "ATL".to_string()]]),
-    //     vec!["JFK".to_string(), "ATL".to_string()]
-    // );
-    // assert_eq!(
-    //     Solution::find_itinerary(vec![
-    //         vec!["JFK".to_string(), "AAA".to_string()],
-    //         vec!["AAA".to_string(), "JFK".to_string()],
-    //         vec!["JFK".to_string(), "BBB".to_string()],
-    //         vec!["JFK".to_string(), "CCC".to_string()],
-    //         vec!["CCC".to_string(), "JFK".to_string()]
-    //     ]),
-    //     vec!["JFK", "AAA", "JFK", "CCC", "JFK", "BBB"]
-    // );
-    // println!(
-    //     "{:?}",
-    //     Solution::find_itinerary(vec![
-    //         vec!["EZE".to_string(), "AXA".to_string()],
-    //         vec!["TIA".to_string(), "ANU".to_string()],
-    //         vec!["ANU".to_string(), "JFK".to_string()],
-    //         vec!["JFK".to_string(), "ANU".to_string()],
-    //         vec!["ANU".to_string(), "EZE".to_string()],
-    //         vec!["TIA".to_string(), "ANU".to_string()],
-    //         vec!["AXA".to_string(), "TIA".to_string()],
-    //         vec!["TIA".to_string(), "JFK".to_string()],
-    //         vec!["ANU".to_string(), "TIA".to_string()],
-    //         vec!["JFK".to_string(), "TIA".to_string()]
-    //     ])
-    // )q;
+    assert_eq!(
+        Solution::find_itinerary(vec![
+            vec!["MUC".to_string(), "LHR".to_string()],
+            vec!["JFK".to_string(), "MUC".to_string()],
+            vec!["SFO".to_string(), "SJC".to_string()],
+            vec!["LHR".to_string(), "SFO".to_string()]
+        ]),
+        vec![
+            "JFK".to_string(),
+            "MUC".to_string(),
+            "LHR".to_string(),
+            "SFO".to_string(),
+            "SJC".to_string()
+        ]
+    );
+    assert_eq!(
+        Solution::find_itinerary(vec![
+            vec!["JFK".to_string(), "SFO".to_string()],
+            vec!["JFK".to_string(), "ATL".to_string()],
+            vec!["SFO".to_string(), "ATL".to_string()],
+            vec!["ATL".to_string(), "JFK".to_string()],
+            vec!["ATL".to_string(), "SFO".to_string()]
+        ]),
+        vec![
+            "JFK".to_string(),
+            "ATL".to_string(),
+            "JFK".to_string(),
+            "SFO".to_string(),
+            "ATL".to_string(),
+            "SFO".to_string()
+        ]
+    );
+    assert_eq!(
+        Solution::find_itinerary(vec![vec!["JFK".to_string(), "ATL".to_string()]]),
+        vec!["JFK".to_string(), "ATL".to_string()]
+    );
+    assert_eq!(
+        Solution::find_itinerary(vec![
+            vec!["JFK".to_string(), "AAA".to_string()],
+            vec!["AAA".to_string(), "JFK".to_string()],
+            vec!["JFK".to_string(), "BBB".to_string()],
+            vec!["JFK".to_string(), "CCC".to_string()],
+            vec!["CCC".to_string(), "JFK".to_string()]
+        ]),
+        vec!["JFK", "AAA", "JFK", "CCC", "JFK", "BBB"]
+    );
+    println!(
+        "{:?}",
+        Solution::find_itinerary(vec![
+            vec!["EZE".to_string(), "AXA".to_string()],
+            vec!["TIA".to_string(), "ANU".to_string()],
+            vec!["ANU".to_string(), "JFK".to_string()],
+            vec!["JFK".to_string(), "ANU".to_string()],
+            vec!["ANU".to_string(), "EZE".to_string()],
+            vec!["TIA".to_string(), "ANU".to_string()],
+            vec!["AXA".to_string(), "TIA".to_string()],
+            vec!["TIA".to_string(), "JFK".to_string()],
+            vec!["ANU".to_string(), "TIA".to_string()],
+            vec!["JFK".to_string(), "TIA".to_string()]
+        ])
+    );
     println!(
         "{:?}",
         Solution::find_itinerary(vec![

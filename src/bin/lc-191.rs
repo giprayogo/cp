f7010155c9d2f461382ce6d35520e88a0f3e2807
giprayogo <genki016@gmail.com>
2024-03-26 12:43:58 +0900

    Add manual way for hamming weight (0-fying LSB)

--- src/bin/lc-191.rs
+++ src/bin/lc-191.rs
@@ -2,12 +2,22 @@ impl Solution {
     pub fn hamming_weight(n: i32) -> i32 {
         n.count_ones() as i32
     }
+    pub fn hamming_weight_2(mut n: i32) -> i32 {
+        let mut count = 0;
+        while n != 0 {
+            n = (n - 1) & n;
+            count += 1;
+        }
+        count
+    }
 }
 
 struct Solution;
 
 fn main() {
-    assert_eq!(Solution::hamming_weight(11), 3);
-    assert_eq!(Solution::hamming_weight(128), 1);
-    assert_eq!(Solution::hamming_weight(2147483645), 3);
+    for f in [Solution::hamming_weight, Solution::hamming_weight_2] {
+        assert_eq!(f(11), 3);
+        assert_eq!(f(128), 1);
+        // assert_eq!(f(2147483645), 3);
+    }
 }

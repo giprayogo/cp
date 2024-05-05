2790b0f05ffaad49841a7f46ba8fb66614075818
giprayogo <genki016@gmail.com>
2024-05-05 20:08:02 +0700

    Easier to read implementation

--- src/bin/algo.rs
+++ src/bin/algo.rs
@@ -4,15 +4,17 @@
 /// Based on https://cp-algorithms.com/string/prefix-function.html.
 ///
 /// Naive function
+#[allow(clippy::needless_range_loop)]
 fn prefix_function_naive(s: &str) -> Vec<i32> {
     let c: Vec<char> = s.chars().collect();
     let n = c.len();
     let mut pi = vec![0; n];
     for i in 0..n {
-        let sub = &c[0..=i];
         'k: for k in 1..(i + 1) {
-            for (f, b) in sub.iter().take(k).zip(sub.iter().skip(i + 1 - k)) {
-                if f != b {
+            let prefix = c.iter().take(k);
+            let suffix = c.iter().skip(i - k + 1).take(k);
+            for (a, b) in prefix.zip(suffix) {
+                if a != b {
                     continue 'k;
                 }
             }

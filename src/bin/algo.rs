63813cfa4120af459a25020ac4b063dc78b30f5c
giprayogo <genki016@gmail.com>
2024-05-07 14:22:17 +0700

    Add optimized prefix function

--- src/bin/algo.rs
+++ src/bin/algo.rs
@@ -5,7 +5,7 @@
 ///
 /// Naive function
 #[allow(clippy::needless_range_loop)]
-fn prefix_function_naive(s: &str) -> Vec<i32> {
+fn prefix_function_naive(s: &str) -> Vec<usize> {
     let c: Vec<char> = s.chars().collect();
     let n = c.len();
     let mut pi = vec![0; n];
@@ -14,26 +14,42 @@ fn prefix_function_naive(s: &str) -> Vec<i32> {
             let prefix = c.iter().take(k);
             let suffix = c.iter().skip(i - k + 1).take(k);
             if prefix.eq(suffix) {
-                pi[i] = k as i32;
+                pi[i] = k;
             }
         }
     }
     pi
 }
 
-fn prefix_function_optimized(s: &str) -> Vec<i32> {
+fn prefix_function_optimized(s: &str) -> Vec<usize> {
     let c: Vec<char> = s.chars().collect();
     let n = c.len();
     let mut pi = vec![0; n];
+    for i in 1..n {
+        let mut j = pi[i - 1];
+        while j > 0 && c[i] != c[j] {
+            // Note that here it is not i - 1 but j - 1!
+            // i.e. I'm finding another prefix/suffix within current best prefix/suffix,
+            // where the suffix property also hold. I was being dense! Paper works!
+            j = pi[j - 1];
+        }
+        if c[i] == c[j] {
+            j += 1;
+        }
+        pi[i] = j;
+    }
     pi
 }
 
 fn main() {
     // assert eq for all the implementations
-    assert_eq!(prefix_function_naive("a"), [0]);
-    assert_eq!(prefix_function_naive("aa"), [0, 1]);
-    assert_eq!(prefix_function_naive("aaa"), [0, 1, 2]);
-    assert_eq!(prefix_function_naive("aaaa"), [0, 1, 2, 3]);
-    assert_eq!(prefix_function_naive("abcabcd"), [0, 0, 0, 1, 2, 3, 0]);
-    assert_eq!(prefix_function_naive("aabaaab"), [0, 1, 0, 1, 2, 2, 3]);
+    for f in [prefix_function_naive, prefix_function_optimized] {
+        println!("{:?}", f("acbdxxxacbb"));
+        assert_eq!(f("a"), [0]);
+        assert_eq!(f("aa"), [0, 1]);
+        assert_eq!(f("aaa"), [0, 1, 2]);
+        assert_eq!(f("aaaa"), [0, 1, 2, 3]);
+        assert_eq!(f("abcabcd"), [0, 0, 0, 1, 2, 3, 0]);
+        assert_eq!(f("aabaaab"), [0, 1, 0, 1, 2, 2, 3]);
+    }
 }

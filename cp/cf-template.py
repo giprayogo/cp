e1755f442c1a7647f2267a28b62357daeeade2cc
giprayogo <genki016@gmail.com>
2024-07-02 21:34:35 +0900

    Update Python CodeForces/AtCoder template

--- cp/cf-template.py
+++ cp/cf-template.py
@@ -2,10 +2,9 @@
 
 
 def main():
-    n = int(input())
-    for _ in range(n):
-        text = input().strip()
-        print(len(text.split()))
+    i = input()
+    if i == "input":
+        print("output")
 
 
 if __name__ == "__main__":
@@ -18,22 +17,17 @@ import sys  # noqa: E402
 
 def test(monkeypatch, capsys):
     # fmt: off
-    mock_input = io.StringIO(
-        "3\n"
-        "a quick brown fox jumps\n"
-        "lorem ipsum\n"
-        "input line 3 !\n"
-    )
+    io_pairs = [
+        (
+            "input\n",
+            "output\n"
+        ),
+    ]
     # fmt: on
-    monkeypatch.setattr(sys, "stdin", mock_input)
 
-    main()
-
-    out, _ = capsys.readouterr()
-    # fmt: off
-    assert out == (
-        "5\n"
-        "2\n"
-        "4\n"
-    )
-    # fmt: on
+    for i, o in io_pairs:
+        mock_input = io.StringIO(i)
+        monkeypatch.setattr(sys, "stdin", mock_input)
+        main()
+        out, _ = capsys.readouterr()
+        assert out == o
